a76c2436ad83919bfd68afe2621935511fee14ce
leon <leon@revillwebdesign.com>
2015-01-28 21:12:17 +0000

    Updates to home page and README

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-angularjs-by-example
-====================
+AngularJS by Example
+===
 
-An example application to demonstrate a wide range of AngularJS best practices
+An application to demonstrate various real world AngularJS best practices and to compliment a tutorial series written on [revillweb.com](http://www.revillweb.com/).
+
+Contributing
+===
+
+If you think that a particular part of this application can be written better or you think this app is missing an important AngularJS concept then please submit a pull request, your help is very welcome.
+
+To do
+===
+
+* Add a contact page to the application to demonstrate form implementation and validation in AngularJS
+* Add tests for the majority of the application and accompanying tutorials
+
+Licence
+===
+
+The application and code are licenced under MIT.
--- src/sections/home/home.ctrl.js
+++ src/sections/home/home.ctrl.js
@@ -7,4 +7,36 @@ angular
         PageValues.description = "Learn AngularJS using best practice real world examples.";
         //Setup view model object
         var vm = this;
+        vm.tutorials = [
+            {
+                title: "INTRODUCTION",
+                description: "An introduction to the AngularJS by example application and the tutorial series. All about why this project exists, what the tutorial series is likely to include and who the tutorials are for.",
+                link: "http://www.google.com/"
+            },
+            {
+                title: "PROJECT STRUCTURE & MODULARITY",
+                description: "Looking at project structure in terms of AngularJS modularity and also how best to lay out your directories to make development a breeze.",
+                link: "#"
+            },
+            {
+                title: "CONTROLLERS",
+                description: "Investigating the different ways you can write AngularJS controllers along with recommended best practices.",
+                link: "#"
+            },
+            {
+                title: "SHARING DATA WITH SERVICES",
+                description: "How to effectively share data around your AngularJS application and when best to use the different types of service available.",
+                link: "#"
+            },
+            {
+                title: "WRITING DIRECTIVES",
+                description: "Theories behind directive design and how to best implement them within your application.",
+                link: "#"
+            },
+            {
+                title: "BUILD PROCESS",
+                description: "So you have an awesome AngularJS app, how do you get it ready for production and deployment? Using Gulp & GitFlow to get the job done.",
+                link: "#"
+            }
+        ];
     });
