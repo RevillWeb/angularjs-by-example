5708500f4d65e178a5bcafc074d3cf6370916ba3
Bohdan Stupak <stupakbogdan@ukr.net>
2018-11-03 18:32:39 +0200

    Translate show directive to typescript

---
+++ src/components/show/show.drct.ts
@@ -0,0 +1,30 @@
+class ShowDirectiveController implements ng.IController {
+    static $inject = ['$scope', 'ShowService']
+    
+    constructor(private $scope, private ShowService : ShowService) {
+        $scope.genres = [];
+        ShowService.get($scope.show.id).then(function(response){
+            $scope.genres = response.genres;
+        });
+        return this;
+    }
+}
+
+class ShowDirective implements ng.IDirective {
+    constructor() {};
+    
+    controller = ShowDirectiveController;    
+    templateUrl = 'components/show/show.tpl.html';
+    restrict = 'E';
+    scope = {
+        show: '='
+    }
+
+    public static Factory(): ng.IDirectiveFactory {
+        return () => new ShowDirective();
+    }
+}
+
+angular
+    .module('app.core')
+    .directive('show', ShowDirective.Factory());
